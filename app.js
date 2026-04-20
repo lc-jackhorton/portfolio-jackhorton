7576cb12ac4a65ec00644b031a3f86c99af15bf5
lc-jackhorton <lc23-0765@lclark.edu>
2026-04-20 16:18:14 -0700

    Fixing app.js to run all files in public

--- app.js
+++ app.js
@@ -1,14 +1,64 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
 const server = http.createServer((req, res) => {
-    fs.readFile("index.html", (err, data)=> {
-        res.writeHead( 200, {"Content-Type": "text/html"});
-        res.write(data);
-        res.end();
-    })
+    if (req.url === "/") {
+        filepath = path.join(__dirname, "public", "index.html");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "text/html"});
+            res.write(data);
+            res.end();
+        })
+    } 
+    else if (req.url === "/styles.css") {
+        filepath = path.join(__dirname, "public", "styles.css");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "text/css"});
+            res.write(data);
+            res.end();
+        })
+    } 
+    else if (req.url === "/IMG_Nature.jpeg") {
+        filepath = path.join(__dirname, "public", "IMG_Nature.jpeg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpeg"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_LC_Circuit.jpeg") {
+        filepath = path.join(__dirname, "public", "IMG_LC_Circuit.jpeg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpeg"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_Kempe.png") {
+        filepath = path.join(__dirname, "public", "IMG_Kempe.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_Portrait.jpg") {
+        filepath = path.join(__dirname, "public", "IMG_Portrait.jpg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpg"});
+            res.write(data);
+            res.end();
+        })
+    }
+        else {
+        res.writeHead(404);
+        res.end("Not found");
+        return;
+    }
 });
 
-server.listen(3000, ()=> {
-    console.log("Server running on http://localhost:3000");
+port=4000
+server.listen(port, ()=> {
+    console.log(`Server running on http://localhost:${port}`);
 });
