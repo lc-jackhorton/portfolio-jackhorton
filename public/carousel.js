397d2f3396a67b39a81791b2ed81dcf1f0160070
lc-jackhorton <lc23-0765@lclark.edu>
2026-04-25 17:50:51 -0700

    Adding carousel to app.js

--- public/carousel.js
+++ public/carousel.js
@@ -1,20 +1,104 @@
-const slides = document.querySelectorAll(".slide");
-const nextbutton = document.querySelector(".nextbutton");
-const previousbutton = document.querySelector(".previousbutton");
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
 
-let currentslide = 0;
-
-function showSlide(index) {
-  slides.forEach(slide => slide.classList.remove("active"));
-  slides[index].classList.add("active");
-}
-
-nextbutton.addEventListener("click", () => {
-  currentslide = (currentslide + 1) % slides.length;
-  showSlide(currentslide);
+const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        filepath = path.join(__dirname, "public", "index.html");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "text/html"});
+            res.write(data);
+            res.end();
+        })
+    } 
+    else if (req.url === "/styles.css") {
+        filepath = path.join(__dirname, "public", "styles.css");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "text/css"});
+            res.write(data);
+            res.end();
+        })
+    } 
+    else if (req.url === "/IMG_Nature.jpeg") {
+        filepath = path.join(__dirname, "public", "IMG_Nature.jpeg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpeg"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_LC_Circuit.jpeg") {
+        filepath = path.join(__dirname, "public", "IMG_LC_Circuit.jpeg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpeg"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_Kempe.png") {
+        filepath = path.join(__dirname, "public", "IMG_Kempe.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_Portrait.jpg") {
+        filepath = path.join(__dirname, "public", "IMG_Portrait.jpg");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/jpg"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/IMG_Installer.png") {
+        filepath = path.join(__dirname, "public", "IMG_Installer.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/current.png") {
+        filepath = path.join(__dirname, "public", "current.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/lccircuit.png") {
+        filepath = path.join(__dirname, "public", "lccircuit.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/solenoid.png") {
+        filepath = path.join(__dirname, "public", "solenoid.png");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "image/png"});
+            res.write(data);
+            res.end();
+        })
+    }
+    else if (req.url === "/carousel.js") {
+        filepath = path.join(__dirname, "public", "carousel.js");
+        fs.readFile(filepath, (err, data)=> {
+            res.writeHead( 200, {"Content-Type": "application/javascript"});
+            res.write(data);
+            res.end();
+        })
+    }
+        else {
+        res.writeHead(404);
+        res.end("Not found");
+        return;
+    }
 });
 
-previousbutton.addEventListener("click", () => {
-  currentslide = (currentslide - 1 + slides.length) % slides.length;
-  showSlide(currentslide);
+port=4000
+server.listen(port, ()=> {
+    console.log(`Server running on http://localhost:${port}`);
 });
